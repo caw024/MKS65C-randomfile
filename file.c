a745b6ed3f051c556a1411d0fab40e4ca69aa9b9
Calvin Aw <caw@stuy.edu>
2018-10-29 18:28:28 -0400

    yay

--- file.c
+++ file.c
@@ -4,12 +4,12 @@
 #include <fcntl.h>
 
 int main(){
-  int fd = open("/dev/random", O_RDWR);
+  int fd = open("/dev/random", O_RDWR | O_CREAT);
 
   printf("Generating random values:\n");
   //populate array
   int arr[10];
-  read(fd, arr, 10*sizeof(int));
+  int l = read(fd, arr, 10*sizeof(int));
   //Help from Hasif
 
   //print out value of array
@@ -17,22 +17,27 @@ int main(){
   for (n; n<10; n++){
     printf("\t random %d: %d\n",n, arr[n]);
   }
+
+  //problem
   
   //write to array
-  int fwd = open("file.txt", O_RDWR | O_CREAT, 777);
-  printf("\nWriting to array\n");
-  write(fwd,arr,10*sizeof(int));
+  int fwd = open("file.txt", O_WRONLY | O_CREAT,777);
+  printf("\nWriting to file\n");
+  int k = write(fwd,arr,10*sizeof(int));
+  int c = close(fwd);
 
   //read file to different array
   int arr2[10];
-  printf("\nReading the array\n\n");
-  read(fwd, arr2, 10*sizeof(int));
+  int wfd = open("file.txt", O_RDONLY);
+  printf("\nReading the file\n\n");
+  int m = read(wfd, arr2, 10*sizeof(int));
+  int w = close(wfd);
+  
 
  //print out value of array
   n = 0;
   for (n; n<10; n++){
     printf("\t random %d: %d\n",n, arr2[n]);
   }
-  
 
 }
